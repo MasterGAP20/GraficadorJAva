f4af26c1e91a0fd496c2c6f02b51016443b60b91
MasterGAP20 <MasterGAP@outlook.com>
2022-05-09 18:49:27 -0500

    Ventana creada (ya le entendi al layout)

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,30 +1,23 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainFrame {
+public class MainFrame extends javax.swing.JFrame{
     public MainFrame() {
-        // Declaracion e inicializacion de variables
-        JFrame jf_ventana = new JFrame("Frame de prueba");
-        JPanel jp_panel = new JPanel();
-        JLabel jl_texto = new JLabel("Esto es un Ejemplo");
-        JButton jb_boton = new JButton();
-        jb_boton.setText("Bomton");
-        // Propiedades panel
-        jp_panel.setLayout(new FlowLayout());
-        jp_panel.setBorder(BorderFactory.createLineBorder(Color.blue));
-        // Asignacion de posiciones
-        jl_texto.setLocation(50, 50);
-        jb_boton.setLocation(150, 150);
-        // Inclusion de componentes
-        jp_panel.add(jl_texto);
-        jp_panel.add(jb_boton);
-        // Propiedades de ventana
-        jf_ventana.setSize(700,600);
-        jf_ventana.add(jp_panel);
-        jf_ventana.setResizable(false);
-        jf_ventana.setLocationRelativeTo(null);
-        jf_ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        jf_ventana.setVisible(true);
+        setTitle("Graficador");
+        jb_boton.setBounds(5,5,120,60);
+        jb_boton.addActionListener((event) -> {
+            System.out.println("Ola");
+        });
+        add(jb_boton);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(800,500);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
 
+    // Declaracionde variables
+    JButton jb_boton = new JButton("Click");
 }
